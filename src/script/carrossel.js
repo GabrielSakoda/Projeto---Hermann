e7f58c6a975f893479ef82679cc9f771ea068893
Gabriel <otanigabriel0@gmail.com>
2024-05-23 13:20:24 -0300

    Adicionando funções novas, modelo 3D de carro, terminando a parte do HOME

--- src/script/carrossel.js
+++ src/script/carrossel.js
@@ -1,6 +1,7 @@
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
     spaceBetween: 10,
+    centeredSlides:true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
